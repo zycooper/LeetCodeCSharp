2cbb2f617eba3774f0f3d8134a913d159aa45945
zycooper <yuanzhang998@gmail.com>
2021-03-21 16:33:02 -0500

    Add Q243 244 245

---
+++ Array/245.Shortest_Word_Distance_III.cs
@@ -0,0 +1,26 @@
+/*
+Given an array of strings wordsDict and two strings that already exist in the array word1 and word2, return the shortest distance between these two words in the list.
+
+Note that word1 and word2 may be the same. It is guaranteed that they represent two individual words in the list.
+
+Example 1:
+Input: wordsDict = ["practice", "makes", "perfect", "coding", "makes"], word1 = "makes", word2 = "coding"
+Output: 1
+
+Example 2:
+Input: wordsDict = ["practice", "makes", "perfect", "coding", "makes"], word1 = "makes", word2 = "makes"
+Output: 3
+
+Constraints:
+
+1 <= wordsDict.length <= 3 * 104
+1 <= wordsDict[i].length <= 10
+wordsDict[i] consists of lowercase English letters.
+word1 and word2 are in wordsDict.
+*/
+
+public class Solution {
+    public int ShortestWordDistance(string[] wordsDict, string word1, string word2) {
+        
+    }
+}
