41a6968dd76269a6f959944c928644fc0b0a3f39
John Zhang <John_Zhang@servms.com>
2021-03-22 15:20:42 -0500

    Add solution for Q 245

--- Array/245.Shortest_Word_Distance_III.cs
+++ Array/245.Shortest_Word_Distance_III.cs
@@ -21,6 +21,61 @@ word1 and word2 are in wordsDict.
 
 public class Solution {
     public int ShortestWordDistance(string[] wordsDict, string word1, string word2) {
-        
+        Dictionary<string, List<int>> _dict = new Dictionary<string, List<int>>();
+
+        //fill up dic
+        for(int i =0; i < wordsDict.Length; i++)
+        {
+            if(_dict.ContainsKey(wordsDict[i]))
+            {
+                _dict[wordsDict[i]].Add(i);
+            }
+            else
+            {
+                _dict[wordsDict[i]]= new List<int>(){i};
+            }
+        }
+
+        int result = int.MaxValue;
+        int index_1 = 0;
+        int index_2 = 0;
+
+        List<int> list_1 = _dict[word1];
+        List<int> list_2 = _dict[word2];
+
+        if(word1 == word2)
+        {
+            for(int i = 0; i < list_1.Count()-1;i++)
+            {
+                int temp_result = Math.Abs(list_1[i] - list_1[i+1]);
+
+                result = Math.Min(temp_result,result);
+            }
+        }
+        else
+        {
+            while(index_1 < list_1.Count() && index_2 < list_2.Count())
+            {
+                int temp_result = Math.Abs(list_1[index_1]-list_2[index_2]);
+
+                result = Math.Min(temp_result,result);
+
+                if(result ==1)
+                {
+                    break;
+                }
+
+                if(list_1[index_1] < list_2[index_2])
+                {
+                    index_1++;
+                }
+                else
+                {
+                    index_2++;
+                }
+            }
+        }
+    
+        return result;
     }
 }
