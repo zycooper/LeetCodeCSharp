8a4fce70fe21622d3f6e9e846754832c6aec8a53
zycooper <yuanzhang998@gmail.com>
2021-03-30 22:49:40 -0500

    Add Q&A252 and Q253

---
+++ Array/252.Meeting_Rooms.cs
@@ -0,0 +1,48 @@
+/*
+Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+
+ 
+
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: false
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: true
+ 
+
+Constraints:
+
+0 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti < endi <= 106
+*/
+
+public class Solution {
+    public bool CanAttendMeetings(int[][] intervals) {
+        //OrderBy(x => x[i])
+        //ToArray()
+        /*
+        Runtime: 112 ms, faster than 32.23% of C# online submissions for Meeting Rooms.
+Memory Usage: 28.1 MB, less than 7.85% of C# online submissions for Meeting Rooms.
+        */
+        if(intervals.Length ==0 || intervals == null)
+        {
+            return true;
+        }
+    //most important below
+        intervals = intervals.OrderBy(x => x[0]).toArray();
+
+        for(int i = 0; i< intervals.Length - 1;i++)
+        {
+            if(intervals[i][1] > intervals[i+1][0])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Array/253.Meeting_Rooms_II.cs
@@ -0,0 +1,49 @@
+/*
+Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
+ 
+
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: 2
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: 1
+ 
+
+Constraints:
+
+1 <= intervals.length <= 104
+0 <= starti < endi <= 106
+*/
+
+public class Solution {
+    public int MinMeetingRooms(int[][] intervals) {
+        //doesn't work below, just write it out but didn't even run it
+        int result = 0;
+
+        if(intervals.Length == 0 || intervals == null)
+        {
+            return result;
+        }
+
+        intervals = intervals.OrderBy(x => x[0]).ToArray();
+
+        for(int i = 0; i < intervals.Length - 1; i++)
+        {
+            if(intervals[i][1] > intervals[i + 1][0])
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+     public int MinMeetingRooms_1(int[][] intervals) 
+     {
+
+     }
+}
