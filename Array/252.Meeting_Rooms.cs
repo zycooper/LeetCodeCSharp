9e7c875bfc287adfa0a7a50160afc7435ff6f329
John Zhang <John_Zhang@servms.com>
2021-04-02 15:13:53 -0500

    add ds note

--- Array/252.Meeting_Rooms.cs
+++ Array/252.Meeting_Rooms.cs
@@ -1,14 +1,11 @@
 /*
 Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
 
- 
-
 Example 1:
-
 Input: intervals = [[0,30],[5,10],[15,20]]
 Output: false
-Example 2:
 
+Example 2:
 Input: intervals = [[7,10],[2,4]]
 Output: true
  
@@ -32,7 +29,7 @@ Memory Usage: 28.1 MB, less than 7.85% of C# online submissions for Meeting Room
         {
             return true;
         }
-    //most important below
+        //most important below
         intervals = intervals.OrderBy(x => x[0]).toArray();
 
         for(int i = 0; i< intervals.Length - 1;i++)
---
+++ Data_Structure_Note.md
@@ -0,0 +1,11 @@
+Major Data Structure:
+1. Array<br>
+2. Linked Lists<br>
+3. Stacks<br>
+4. Queues<br>
+5. Hash Tables<br>
+6. Trees<br>
+7. Heaps (special case of a binary tree)<br>
+8. Graphs<br>
+
+https://towardsdatascience.com/8-common-data-structures-every-programmer-must-know-171acf6a1a42
