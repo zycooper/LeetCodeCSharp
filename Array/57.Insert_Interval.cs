7c34b61e09096db229affb91924ab23f2dfc6fd5
John Zhang <John_Zhang@servms.com>
2021-04-08 17:01:09 -0500

    tt

---
+++ Array/57.Insert_Interval.cs
@@ -0,0 +1,117 @@
+/*
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+Example 2:
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+Example 3:
+Input: intervals = [], newInterval = [5,7]
+Output: [[5,7]]
+
+Example 4:
+Input: intervals = [[1,5]], newInterval = [2,3]
+Output: [[1,5]]
+
+Example 5:
+Input: intervals = [[1,5]], newInterval = [2,7]
+Output: [[1,7]]
+
+Constraints:
+0 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= intervals[i][0] <= intervals[i][1] <= 105
+intervals is sorted by intervals[i][0] in ascending order.
+newInterval.length == 2
+0 <= newInterval[0] <= newInterval[1] <= 105
+*/
+
+/*
+Greedy problems usually look like "Find minimum number of something to do something" or "Find maximum number of something to fit in some conditions", and typically propose an unsorted input.
+*/
+
+class Solution {
+  public int[][] insert(int[][] intervals, int[] newInterval) {
+    // init data
+    int newStart = newInterval[0], newEnd = newInterval[1];
+    int idx = 0, n = intervals.length;
+    LinkedList<int[]> output = new LinkedList<int[]>();
+
+    // add all intervals starting before newInterval
+    while (idx < n && newStart > intervals[idx][0])
+      output.add(intervals[idx++]);
+
+    // add newInterval
+    int[] interval = new int[2];
+    // if there is no overlap, just add the interval
+    if (output.isEmpty() || output.getLast()[1] < newStart)
+      output.add(newInterval);
+    // if there is an overlap, merge with the last interval
+    else {
+      interval = output.removeLast();
+      interval[1] = Math.max(interval[1], newEnd);
+      output.add(interval);
+    }
+
+    // add next intervals, merge with newInterval if needed
+    while (idx < n) {
+      interval = intervals[idx++];
+      int start = interval[0], end = interval[1];
+      // if there is no overlap, just add an interval
+      if (output.getLast()[1] < start) output.add(interval);
+      // if there is an overlap, merge with the last interval
+      else {
+        interval = output.removeLast();
+        interval[1] = Math.max(interval[1], end);
+        output.add(interval);
+      }
+    }
+    return output.toArray(new int[output.size()][2]);
+  }
+}
+
+public class Solution {
+    public int[][] Insert(int[][] intervals, int[] newInterval) {
+        if(intervals.Length ==0 )
+        {
+            return new int[][]{newInterval};
+        }
+
+        int start_pos = 0;
+        int end_pos= 0;
+
+        for(int row = 0; row < intervals.Length; row++)
+        {
+            if(intervals[row][0] <= newInterval[0])
+            {
+                start_pos = row;
+            }
+
+            if(intervals[row][1] >= newInterval[1] && end_pos == 0)
+            {
+                //make sure assign the first match value to end_pos
+                end_pos = row;
+            }
+        }
+
+        int[,] Output = new int[intervals.Length - (end_pos - start_pos),2];
+
+        for(int i = 0; i < intervals.Length; i++)
+        {
+            if(intervals[i][1] < start_pos)
+            {
+                //before start point
+            }
+            else if(intervals[i][0] >= start_pos)
+            {
+                //
+            }
+        }
+    }
+}
