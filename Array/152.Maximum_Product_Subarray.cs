53ba2ec74a1edb651fe018fe5750ae919d9f9fbc
John Zhang <John_Zhang@servms.com>
2021-04-12 16:30:38 -0500

    add Q152 and wrong A152

--- Array/152.Maximum_Product_Subarray.cs
+++ Array/152.Maximum_Product_Subarray.cs
@@ -1,3 +1,68 @@
 /*
-test
+Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
+It is guaranteed that the answer will fit in a 32-bit integer.
+A subarray is a contiguous subsequence of the array.
+
+Example 1:
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+
+Example 2:
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+Constraints:
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 */
+
+ /********************************************************************************
+ Attampt Times: 2
+ *********************************************************************************
+ Time Range:
+ From: 2021-04-12 15:56
+ To: 2021-04-12 15:28
+ *********************************************************************************
+ Submission Result:
+
+ *********************************************************************************
+ Note: 
+
+ *********************************************************************************/
+ public class Solution {
+     public int MaxProduct(int[] nums) {
+
+       //need to add one more param min_curr since the negative could be maximum if product by another negative
+        int[] max_cur = new int[nums.Length];
+        int[] min_cur = new int[nums.Length];
+
+        max_cur[0] = nums[0];
+        min_cur[0] = nums[0];
+
+        for(int i = 1; i < nums.Length;i++)
+        {
+            max_cur[i] = Math.Max(Math.Max(nums[i] * max_cur[i-1], max_cur[i-1]),nums[i]);
+            min_cur[i] = Math.Min(Math.Min(nums[i] * min_cur[i-1], min_cur[i-1]),nums[i]);
+        }
+
+        return max_cur.Max();
+    }
+
+     //doesn't work because the first element could be negative
+    public int MaxProduct_old(int[] nums) {
+        int max_subArray = 1;
+        int cur_subArray = 1;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            cur_subArray *= nums[i];
+
+            max_subArray = Math.Max(max_subArray,cur_subArray);
+        }
+
+        return max_subArray;
+    }
+}
