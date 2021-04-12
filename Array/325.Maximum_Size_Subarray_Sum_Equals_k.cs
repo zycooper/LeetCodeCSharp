40d875b3eaf14fdaba6f827b950fe8127b5d830f
John Zhang <John_Zhang@servms.com>
2021-04-12 11:38:23 -0500

    Add default comment note layout to all empty files

---
+++ Array/325.Maximum_Size_Subarray_Sum_Equals_k.cs
@@ -0,0 +1,31 @@
+/*
+Given an integer array nums and an integer k, return the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
+
+Example 1:
+Input: nums = [1,-1,5,-2,3], k = 3
+Output: 4
+Explanation: The subarray [1, -1, 5, -2] sums to 3 and is the longest.
+
+Example 2:
+Input: nums = [-2,-1,2,1], k = 1
+Output: 2
+Explanation: The subarray [-1, 2] sums to 1 and is the longest.
+ 
+Constraints:
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+-105 <= k <= 105
+*/
+    /********************************************************************************
+    Attampt Index: 0
+    *********************************************************************************
+    Time Range:
+    From: 
+    To: 
+    *********************************************************************************
+    Submission Result:
+    
+    *********************************************************************************
+    Note:
+    
+    *********************************************************************************/
