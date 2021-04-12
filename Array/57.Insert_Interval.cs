81846e01ef6438db10df539c3cbb2af0497f6d50
John Zhang <John_Zhang@servms.com>
2021-04-12 08:49:43 -0500

    tab

--- Array/57.Insert_Interval.cs
+++ Array/57.Insert_Interval.cs
@@ -2,6 +2,8 @@
 Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
 You may assume that the intervals were initially sorted according to their start times.
 
+tab 
+
 Example 1:
 Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
 Output: [[1,5],[6,9]]
