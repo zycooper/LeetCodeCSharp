26de026038c1de8d18b09522c186165d080b93e2
John Zhang <John_Zhang@servms.com>
2021-04-12 11:40:06 -0500

    add default comment layout file

---
+++ default_comment.cs
@@ -0,0 +1,36 @@
+class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] lines =
+           {
+                "/*", "", "*/","",
+                " /********************************************************************************",
+                " Attampt Times: 0",
+                " *********************************************************************************",
+                " Time Range:",
+                " From: ",
+                " To: ",
+                " *********************************************************************************",
+                " Submission Result:",
+                "",
+                " *********************************************************************************",
+                " Note: ",
+                "",
+                " *********************************************************************************/",
+            };
+
+            List<string> list = new List<string>();
+            foreach (string dirFile in Directory.GetDirectories(@"C:\Users\T1703001\Desktop\LC\"))
+            {
+                foreach (string path in Directory.GetFiles(dirFile))
+                {
+                    // fileName  is the file name
+                    if (new System.IO.FileInfo(path).Length ==0)
+                    {
+                        File.WriteAllLines(path, lines);
+                    }
+                }
+            }                
+        }
+    }
