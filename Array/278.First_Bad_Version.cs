0fe42795cca2467ed0fbe863f842bde511847344
John Zhang <John_Zhang@servms.com>
2021-04-14 15:57:10 -0500

    add Q&A 278

--- Array/278.First_Bad_Version.cs
+++ Array/278.First_Bad_Version.cs
@@ -1,17 +1,61 @@
 /*
+You are a product manager and currently leading a team to develop a new product. 
+Unfortunately, the latest version of your product fails the quality check. 
+Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+You are given an API bool isBadVersion(version) which returns whether version is bad. 
+Implement a function to find the first bad version. You should minimize the number of calls to the API.
 
+Example 1:
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version.
+
+Example 2:
+Input: n = 1, bad = 1
+Output: 1
 */
-
  /********************************************************************************
- Attampt Times: 0
+ Attampt Times: 1
  *********************************************************************************
  Time Range:
- From: 
- To: 
+ From: 2021-04-14 15:15
+ To: 2021-04-14 15:56
  *********************************************************************************
  Submission Result:
-
+    Runtime: 40 ms, faster than 59.35% of C# online submissions for First Bad Version.
+    Memory Usage: 15.2 MB, less than 65.62% of C# online submissions for First Bad Version.
  *********************************************************************************
  Note: 
-
+    typical mid sort
  *********************************************************************************/
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public class Solution : VersionControl {
+    public int FirstBadVersion(int n) {
+        int left = 0;
+        int right = n;
+
+        while(left < right)
+        {
+            int mid = left + (right-left)/2;
+
+            if(IsBadVersion(mid))
+            {
+                //right part all true
+                right = mid;
+            }
+            else
+            {
+                left = mid +1;
+            }
+        }
+
+        return left;
+    }
+}
