e3511ca88e0675bccb96143b3dedcbbfa11af5b1
John Zhang <John_Zhang@servms.com>
2021-04-14 16:14:41 -0500

    add Q&A35

--- Array/35.Search_Insert_Position.cs
+++ Array/35.Search_Insert_Position.cs
@@ -1,17 +1,71 @@
 /*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
 
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+Example 4:
+Input: nums = [1,3,5,6], target = 0
+Output: 0
+
+Example 5:
+Input: nums = [1], target = 0
+Output: 0
+ 
+Constraints:
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
 */
 
  /********************************************************************************
- Attampt Times: 0
+ Attampt Times: 1
  *********************************************************************************
  Time Range:
- From: 
- To: 
+ From: 2021-04-14 16:04
+ To: 2021-04-14 16:13
  *********************************************************************************
  Submission Result:
 
  *********************************************************************************
  Note: 
-
+Runtime: 88 ms, faster than 90.59% of C# online submissions for Search Insert Position.
+Memory Usage: 25.4 MB, less than 33.40% of C# online submissions for Search Insert Position.
  *********************************************************************************/
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length -1;
+        int mid = 0;
+        while(left <= right)
+        {
+            mid = left + (right - left)/2;
+
+            if(nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else if(nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+
+        return left;
+    }
+}
