cfd4344f1256bdfd5e4d7241f697aa65fc33da2e
John Zhang <John_Zhang@servms.com>
2021-04-21 10:26:12 -0500

    add Q&A309

--- Array/309.Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cs
+++ Array/309.Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cs
@@ -24,21 +24,54 @@ Constraints:
  *********************************************************************************
  Time Range:
  From: 
- To: 
+ To: 2021-04-21 10:25
  *********************************************************************************
  Submission Result:
-https://en.wikipedia.org/wiki/Finite-state_machine finite-state machine
+    
+    Runtime: 96 ms, faster than 45.71% of C# online submissions for Best Time to Buy and Sell Stock with Cooldown.
+Memory Usage: 25.3 MB, less than 43.81% of C# online submissions for Best Time to Buy and Sell Stock with Cooldown.
  *********************************************************************************
  Note: 
     DP
+    https://en.wikipedia.org/wiki/Finite-state_machine finite-state machine
+
+    two state: hold and unhold
+    hold:max-profit if by the end of ith day we hold stock
+    unhold:max-profit if by the end of ith day we DON'T hold stock
+
+    transation situation:P
+
+    if hold:
+    1. Buy stock today          ----->  unhold[i-2] - prices[i] ----->  even if you sell stock more than i-2, unhold[i-2] will be the same with with unhold[i-n] or unhold[i-1] because unhold it anyway
+    2. Didn't buy stock today   ----->  hold[i-1]               ----->
+
+    if unhold:
+    1. Sell stock today         ----->  hold[i-1] + prices[i]   ----->
+    2. Didn't sell stock today  ----->  unhold[i-1]             ----->
  *********************************************************************************/
 
 public class Solution {
-    public int MaxProfit(int[] prices) {
-        
+    public int MaxProfit(int[] prices) {              
+        if(prices == null || prices.Length <= 1){return 0;}
+        int[] hold = new int[prices.Length];
+        int[] unhold = new int[prices.Length];
+
+        //initial hold and unhold
+        hold[0] = -prices[0];
+        hold[1] = Math.Max(-prices[0],-prices[1]);
+
+        unhold[0] = 0;
+
+        for(int i = 1;i < prices.Length;i++)
+        {
+            if(i > 1)
+            {
+                hold[i] = Math.Max(unhold[i-2] - prices[i] , hold[i-1]);
+            }
+
+            unhold[i] = Math.Max(hold[i-1] + prices[i] , unhold[i-1]);
+        }
+
+        return unhold[unhold.Length - 1];
     }
 }
-
-/*
-tab
-*/
