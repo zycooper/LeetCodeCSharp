4b7e18e2da551f4a4e3d795280ebd8c4a675f0f7
John Zhang <John_Zhang@servms.com>
2021-04-28 11:07:08 -0500

    add Q&A 290 and 242

--- String/242.Valid_Anagram.cs
+++ String/242.Valid_Anagram.cs
@@ -1,17 +1,74 @@
 /*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+ 
+Constraints:
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
 */
 
  /********************************************************************************
  Solution Category: 0
  *********************************************************************************
  Time Range:
- From: 
- To: 
+ From: 2021-04-28 11:00
+ To: 2021-04-28 11:06
  *********************************************************************************
  Submission Result:
-
+Runtime: 88 ms, faster than 56.38% of C# online submissions for Valid Anagram.
+Memory Usage: 24.4 MB, less than 73.90% of C# online submissions for Valid Anagram.
  *********************************************************************************
  Note: 
+    for specific char with index of string: string[index]
 
+    need to get more familir with dictionary function and property
+    ElementAt
+    
  *********************************************************************************/
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != 0 && t.Length !=0 && s.Length != t.Length ){ return false;}
+
+        var dict_s = dictionary_generator(s);
+        var dict_t = dictionary_generator(t);
+
+        if(dict_s.Count() != dict_t.Count()){return false;}
+
+        for(int i =0; i < dict_s.Count();i ++)
+        {
+            if (!dict_t.ContainsKey(dict_s.ElementAt(i).Key) || dict_t[dict_s.ElementAt(i).Key] != dict_s.ElementAt(i).Value)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private Dictionary<char,int> dictionary_generator(string str)
+    {
+        Dictionary<char,int> result = new Dictionary<char, int>();
+
+        for(int i =0; i < str.Length; i++)
+        {
+            if(result.ContainsKey(str[i]))
+            {
+                result[str[i]] += 1;
+            }
+            else
+            {
+                result.Add(str[i],1);
+            }
+        }
+
+        return result;
+    }
+}
--- String/290.Word_Pattern.cs
+++ String/290.Word_Pattern.cs
@@ -1,17 +1,77 @@
 /*
+Given a pattern and a string s, find if s follows the same pattern.
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
 
+Example 1:
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+Example 2:
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+Example 3:
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+Example 4:
+Input: pattern = "abba", s = "dog dog dog dog"
+Output: false
+ 
+
+Constraints:
+1 <= pattern.length <= 300
+pattern contains only lower-case English letters.
+1 <= s.length <= 3000
+s contains only lower-case English letters and spaces ' '.
+s does not contain any leading or trailing spaces.
+All the words in s are separated by a single space.
 */
 
  /********************************************************************************
  Solution Category: 0
  *********************************************************************************
  Time Range:
- From: 
- To: 
+ From: 2021-04-28 10:32
+ To: 2021-04-28 10:44
  *********************************************************************************
  Submission Result:
-
+    Runtime: 84 ms, faster than 22.73% of C# online submissions for Word Pattern.
+    Memory Usage: 22.7 MB, less than 51.70% of C# online submissions for Word Pattern.
  *********************************************************************************
  Note: 
+same with Q205
 
+ dict_pattern.ContainsKey(arr_pattern[i]) != dict_s.ContainsKey(arr_s[i]) ----> stored pattern not match
+ (dict_pattern.ContainsKey(arr_pattern[i]) && dict_s.ContainsKey(arr_s[i]) && dict_pattern[arr_pattern[i]] != dict_s[arr_s[i]]) -----> both element stored but map to different element
  *********************************************************************************/
+public class Solution {
+    public bool WordPattern(string pattern, string s) {
+        Dictionary<char,int> dict_pattern = new Dictionary<char, int>();
+        Dictionary<string,int> dict_s = new Dictionary<string, int>();
+
+        char[] arr_pattern = pattern.ToCharArray();
+        string[] arr_s = s.Split(' ');
+
+        if(arr_pattern.Length == 0 && arr_s.Length ==0){return true;}
+        if(arr_pattern.Length != arr_s.Length){ return false;}
+
+        for(int i = 0; i < arr_pattern.Length; i++)
+        {
+            //not match
+            //
+            if(
+                dict_pattern.ContainsKey(arr_pattern[i]) != dict_s.ContainsKey(arr_s[i])
+            || (dict_pattern.ContainsKey(arr_pattern[i]) && dict_s.ContainsKey(arr_s[i]) && dict_pattern[arr_pattern[i]] != dict_s[arr_s[i]])
+            )
+            {
+                return false;
+            }
+            //add to dict
+            if(!dict_pattern.ContainsKey(arr_pattern[i]))
+            {
+                dict_pattern.Add(arr_pattern[i],i);
+                dict_s.Add(arr_s[i],i);
+            }
+        }
+
+        return true;
+    }
+}
