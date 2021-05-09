9b36c651f5db9711df53ccba1e04020a69de29b5
zycooper <yuanzhang998@gmail.com>
2021-05-09 15:17:59 -0500

    Update 161.One_Edit_Distance.cs

--- String/161.One_Edit_Distance.cs
+++ String/161.One_Edit_Distance.cs
@@ -39,11 +39,64 @@ s and t consist of lower-case letters, upper-case letters and/or digits.
  To: 
  *********************************************************************************
  Submission Result:
-
+Runtime: 64 ms, faster than 98.29% of C# online submissions for One Edit Distance.
+Memory Usage: 23.9 MB, less than 18.86% of C# online submissions for One Edit Distance.
  *********************************************************************************
  Note: 
-
+ missing too many corner cases.
  *********************************************************************************/
+
+public class Solution {
+ //this one works
+    public bool IsOneEditDistance(string s, string t) {
+        //check length diff
+     
+        if(s == t)
+        {
+            return false;
+        }
+        else if(Math.Abs(s.Length - t.Length) < 2)
+        {
+            //loop through the not long one
+            string longStr ="";
+            string shortStr ="";
+            if(s.Length > t.Length)
+            {
+                longStr = s;
+                shortStr =t;
+            }
+            else
+            {
+                shortStr =s;
+                longStr = t;
+            }
+          
+            if(shortStr.Length == 0 && longStr.Length == 1){ return true;}
+            
+            for(int i = 0; i < shortStr.Length; i++)
+            {
+                if(shortStr[i] != longStr[i])
+                {    
+                    if(s.Length == t.Length)
+                    {
+                        return shortStr.Substring(i+1) == longStr.Substring(i+1);
+                    }
+                    else
+                    {
+                        return shortStr.Substring(i) == longStr.Substring(i+1);
+                    }
+                }   
+            }
+         //after loop through and no different character found, the the last one is the one, so return true
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
 public class Solution {
     public bool IsOneEditDistance(string s, string t) {
         if(s == t || (string.IsNullOrEmpty(s) && string.IsNullOrEmpty(t)) || Math.Abs(s.Length - t.Length) > 1 ){ return false;}
