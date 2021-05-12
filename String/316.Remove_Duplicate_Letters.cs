9b27f57c5b738fbb017b845572e6552c21cb9985
John Zhang <John_Zhang@servms.com>
2021-05-12 17:00:53 -0500

    add q316

--- String/316.Remove_Duplicate_Letters.cs
+++ String/316.Remove_Duplicate_Letters.cs
@@ -1,12 +1,25 @@
 /*
+Given a string s, remove duplicate letters so that every letter appears once and only once. 
+You must make sure your result is the smallest in lexicographical order among all possible results.
+Note: This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
 
+Example 1:
+Input: s = "bcabc"
+Output: "abc"
+
+Example 2:
+Input: s = "cbacdcbc"
+Output: "acdb"
+
+Constraints:
+1 <= s.length <= 104
+s consists of lowercase English letters.
 */
-
  /********************************************************************************
  Solution Category: 0
  *********************************************************************************
  Time Range:
- From: 
+ From: 2021-05-12 15:34
  To: 
  *********************************************************************************
  Submission Result:
@@ -15,3 +28,8 @@
  Note: 
 
  *********************************************************************************/
+public class Solution {
+    public string RemoveDuplicateLetters(string s) {
+        
+    }
+}
