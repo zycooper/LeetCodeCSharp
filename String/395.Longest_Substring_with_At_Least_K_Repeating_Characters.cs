68e2aab12c08c3584e2878b59746fb320eb072b1
zycooper <yuanzhang998@gmail.com>
2021-05-23 16:07:42 -0500

    Update 395.Longest_Substring_with_At_Least_K_Repeating_Characters.cs

--- String/395.Longest_Substring_with_At_Least_K_Repeating_Characters.cs
+++ String/395.Longest_Substring_with_At_Least_K_Repeating_Characters.cs
@@ -36,3 +36,78 @@ s consists of only lowercase English letters.
  Note: 
 
  *********************************************************************************/
+public class Solution {
+ //first try, doesn't work on "bbaaacb" - 3
+    public int LongestSubstring(string s, int k) {
+        //5/23/2021 first try:
+        /*
+         1. create a map store each char and it's occrurent time
+         2. chekc all the substring delimit by the char which has the un-qulified ocurrent time
+         3. return the longest length among these substrings
+        */
+        
+        /*
+        After check the Divide and Conquer
+        */
+        //pre-condition
+        if(s.Length < k){ return 0;}
+        if(s.Length < 2){ return s.Length;}
+        
+        //dict could be replaced by a int[] because each char has it's own index
+        //Dictionary<char,int> dict = new Dictionary<char,int>();
+        int[] dict = new int[26];
+        int res = 0;
+        
+        
+        for(int i = 0; i < s.Length; i++)
+        {
+            dict[s[i] - 'a']++;
+        }
+        
+        int left = 0;
+        int right = 0;
+        
+        for(int j = 0; j < s.Length; j++)
+        {
+            if(dict[s[j] - 'a'] >= k)
+            {
+                //not delimiter
+                right = j;
+            }
+            else if(j != s.Length - 1)
+            {
+                //reset left and right
+                left = j + 1;
+                right = j + 1;
+            }
+            
+            res = Math.Max(res,lengthCheck(s.Substring(left, right - left +1),k));
+        }
+        
+        return res;
+    }
+    
+    private int lengthCheck(string s, int k)
+    {
+        int[] dict= new int[26];
+        for(int i = 0; i < s.Length; i++)
+        {
+            dict[s[i] - 'a']++;
+        }
+        
+        for(int j = 0; j < dict.Length; j++)
+        {
+            if(dict[j] != 0 && dict[j] < k)
+            {
+                return 0;
+            }
+        }
+        
+        return s.Length;
+    }
+}
+
+/*
+abbbb && 4 -> bbbb
+aaaab && 4 -> aaaa
+*/
