7814dd8a152d9ecaa9fbe50e2e82bff0287fd4f3
John Zhang <John_Zhang@servms.com>
2021-05-24 15:57:20 -0500

    add Q&A9

--- String/266.Palindrome_Permutation.cs
+++ String/266.Palindrome_Permutation.cs
@@ -1,17 +1,143 @@
 /*
+Given a string s, return true if a permutation of the string could form a palindrome.
 
+ 
+
+Example 1:
+
+Input: s = "code"
+Output: false
+Example 2:
+
+Input: s = "aab"
+Output: true
+Example 3:
+
+Input: s = "carerac"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 5000
+s consists of only lowercase English letters.
 */
 
  /********************************************************************************
  Solution Category: 0
  *********************************************************************************
  Time Range:
- From: 
- To: 
+ From: 2021-05-24 15:42
+ To: 2021-05-24 15:48
  *********************************************************************************
  Submission Result:
-
+Runtime: 76 ms, faster than 46.93% of C# online submissions for Palindrome Permutation.
+Memory Usage: 22.2 MB, less than 75.45% of C# online submissions for Palindrome Permutation.
  *********************************************************************************
  Note: 
 
+
  *********************************************************************************/
+ public class Solution {
+     /*
+     Runtime: 72 ms, faster than 74.01% of C# online submissions for Palindrome Permutation.
+     Memory Usage: 22.6 MB, less than 19.49% of C# online submissions for Palindrome Permutation.    
+     */
+    public bool CanPermutePalindrome(string s) {
+        //pre-condition
+        
+        //normal condition
+        int[] dict = new int[26];
+        
+        for(int i = 0; i < s.Length; i++)
+        {
+            dict[s[i] - 'a']++;
+        }
+        
+        //below is optimize one
+        int count =0;
+        for(int i = 0; i < 26; i++)
+        {
+            count += dict[i]%2;
+        }
+        
+        return count <=1;
+        /*
+        //below is the first time submit, 48% speed
+        if(s.Length%2 ==0)
+        {
+            //even
+            for(int i = 0; i < 26; i++)
+            {
+                if(dict[i] > 0 && dict[i]%2 !=0)
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            //odd
+            bool oddused = false;
+            
+            for(int i = 0; i < 26; i++)
+            {
+                if(dict[i] > 0 && dict[i]%2 !=0 && oddused)
+                {
+                    return false;   
+                }
+                else if(dict[i] > 0 && dict[i]%2 !=0 && !oddused)
+                {
+                    oddused = true;
+                }
+            }
+        }
+        */
+        
+        return true;
+    }
+}
+public class Solution {
+    public bool CanPermutePalindrome(string s) {
+        //pre-condition
+        
+        //normal condition
+        int[] dict = new int[26];
+        
+        for(int i = 0; i < s.Length; i++)
+        {
+            dict[s[i] - 'a']++;
+        }
+        
+        if(s.Length%2 ==0)
+        {
+            //even
+            for(int i = 0; i < 26; i++)
+            {
+                if(dict[i] > 0 && dict[i]%2 !=0)
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            //odd
+            bool oddused = false;
+            
+            for(int i = 0; i < 26; i++)
+            {
+                if(dict[i] > 0 && dict[i]%2 !=0 && oddused)
+                {
+                    return false;   
+                }
+                else if(dict[i] > 0 && dict[i]%2 !=0 && !oddused)
+                {
+                    oddused = true;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
--- String/9.Palindrome_Number.cs
+++ String/9.Palindrome_Number.cs
@@ -6,12 +6,38 @@ question
  Solution Category: 0
  *********************************************************************************
  Time Range:
- From: 
- To: 
+ From: 2021-05-24 15:50
+ To: 2021-05-24 15:55
  *********************************************************************************
  Submission Result:
-
+Runtime: 68 ms, faster than 50.30% of C# online submissions for Palindrome Number.
+Memory Usage: 17 MB, less than 50.26% of C# online submissions for Palindrome Number.
  *********************************************************************************
  Note: 
-
+modified from Q125 very easy one
  *********************************************************************************/
+public class Solution {
+    public bool IsPalindrome(int x) {
+        string s = x.ToString();
+        if(string.IsNullOrEmpty(s)){ return false;}        
+        if(s.Length == 1){ return true;}
+        
+        int left = 0;
+        int right = s.Length - 1;
+        
+        while(left <= right)
+        {
+                //both left and right position char is letter
+                if(s[left] != s[right])
+                {
+                    return false;
+                }
+                
+                left++;
+                right--;
+            
+        }
+        
+        return true;
+    }
+}
