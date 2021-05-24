075ab843e2d27bd4a3e044dda28f44e135056985
John Zhang <John_Zhang@servms.com>
2021-05-24 15:40:19 -0500

    add Q&A125

--- String/125.Valid_Palindrome.cs
+++ String/125.Valid_Palindrome.cs
@@ -3,15 +3,55 @@
 */
 
  /********************************************************************************
- Solution Category: 0
+ Solution Category: 
+ Two Pointers
  *********************************************************************************
  Time Range:
- From: 
- To: 
+ From: 2021-05-24 15:27
+ To: 2021-05-24 15:38
  *********************************************************************************
  Submission Result:
-
+Runtime: 76 ms, faster than 84.42% of C# online submissions for Valid Palindrome.
+Memory Usage: 24.6 MB, less than 89.13% of C# online submissions for Valid Palindrome.
  *********************************************************************************
  Note: 
-
+use IsLetterOrDigit instead of IsLetter!!!!!
+Char.IsLetterOrDigit('')
+Char.IsLetter('')
+Char.ToLower(s[left])
  *********************************************************************************/
+public class Solution {
+    public bool IsPalindrome(string s) {
+        if(string.IsNullOrEmpty(s)){ return false;}
+        
+        if(s.Length == 1){ return true;}
+        
+        int left = 0;
+        int right = s.Length -1;
+        
+        while(left <= right)
+        {
+            if(!Char.IsLetterOrDigit (s[left]))
+            {
+                left++;
+            }
+            else if(!Char.IsLetterOrDigit (s[right]))
+            {
+                right--;
+            }
+            else
+            {
+                //both left and right position char is letter
+                if(Char.ToLower(s[left]) != Char.ToLower(s[right]) )
+                {
+                    return false;
+                }
+                
+                left++;
+                right--;
+            }
+        }
+        
+        return true;
+    }
+}
