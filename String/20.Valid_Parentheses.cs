06ed452f19bb7b6b1e274391af67b42eb0a61404
John Zhang <John_Zhang@servms.com>
2021-05-26 16:25:42 -0500

    add Q&A20

--- String/20.Valid_Parentheses.cs
+++ String/20.Valid_Parentheses.cs
@@ -1,17 +1,78 @@
 /*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+ 
 
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+
+Example 4:
+Input: s = "([)]"
+Output: false
+
+Example 5:
+Input: s = "{[]}"
+Output: true
+
+Constraints:
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
 */
 
  /********************************************************************************
- Solution Category: 0
+ Solution Category: 
+ Stack
  *********************************************************************************
  Time Range:
- From: 
- To: 
+ From: 2021-05-26 16:10
+ To: 2021-05-26 16:23
  *********************************************************************************
  Submission Result:
 
  *********************************************************************************
  Note: 
-
+Stack
+need to know:
+prop: Count
+func: Pop() Push() Peak()
  *********************************************************************************/
+public class Solution {
+    public bool IsValid(string s) {
+         Stack<char> sign = new Stack<char>();
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(s[i] == '(')
+            {
+                sign.Push(')');
+            }
+            else if(s[i] == '[')
+            {
+                sign.Push(']');
+            }
+            else if(s[i] == '{')
+            {
+                sign.Push('}');
+            }
+            else if(sign.Count == 0 || sign.Pop() != s[i])
+            {
+                return false;
+            }
+        }
+
+        //cannot return true directly, for "["
+        return sign.Count == 0;
+    }
+}
