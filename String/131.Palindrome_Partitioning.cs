634e3e37862f64910fba92bf60d73725894acf6e
John Zhang <John_Zhang@servms.com>
2021-05-27 15:48:12 -0500

    add note

--- String/131.Palindrome_Partitioning.cs
+++ String/131.Palindrome_Partitioning.cs
@@ -1,12 +1,26 @@
 /*
+Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+A palindrome string is a string that reads the same backward as forward.
 
+Example 1:
+
+Input: s = "aab"
+Output: [["a","a","b"],["aa","b"]]
+Example 2:
+Input: s = "a"
+Output: [["a"]]
+
+Constraints:
+1 <= s.length <= 16
+s contains only lowercase English letters.
 */
 
- /********************************************************************************
- Solution Category: 0
+ /*********************************** *********************************************
+ Solution Category: 
+ Backtracking | DFS(Depth-First-Search)
  *********************************************************************************
  Time Range:
- From: 
+ From: 2021-05-27 14:51
  To: 
  *********************************************************************************
  Submission Result:
@@ -15,3 +29,8 @@
  Note: 
 
  *********************************************************************************/
+public class Solution {
+    public IList<IList<string>> Partition(string s) {
+        
+    }
+}
--- String/65.Valid_Number.cs
+++ String/65.Valid_Number.cs
@@ -14,32 +14,28 @@ An integer can be split up into these components (in order):
 
 (Optional) A sign character (either '+' or '-').
 At least one digit.
-For example, all the following are valid numbers: ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"], while the following are not valid numbers: ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"].
+For example, all the following are valid numbers: ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"], 
+while the following are not valid numbers: ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"].
 
 Given a string s, return true if s is a valid number.
 
- 
-
 Example 1:
-
 Input: s = "0"
 Output: true
-Example 2:
 
+Example 2:
 Input: s = "e"
 Output: false
-Example 3:
 
+Example 3:
 Input: s = "."
 Output: false
-Example 4:
 
+Example 4:
 Input: s = ".1"
 Output: true
- 
 
 Constraints:
-
 1 <= s.length <= 20
 s consists of only English letters (both uppercase and lowercase), digits (0-9), plus '+', minus '-', or dot '.'.
 */
