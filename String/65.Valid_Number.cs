29835d3fbff345995c8f75c2cf3cb4ee80865be3
John Zhang <John_Zhang@servms.com>
2021-06-02 15:13:18 -0500

    add A65

--- String/65.Valid_Number.cs
+++ String/65.Valid_Number.cs
@@ -45,16 +45,70 @@ s consists of only English letters (both uppercase and lowercase), digits (0-9),
  *********************************************************************************
  Time Range:
  From: 2021-05-19 10:17
- To: 
+ To: 2021-06-02 15:11
  *********************************************************************************
  Submission Result:
-
+Runtime: 116 ms, faster than 21.51% of C# online submissions for Valid Number.
+Memory Usage: 26 MB, less than 94.19% of C# online submissions for Valid Number.
  *********************************************************************************
  Note: 
 
  *********************************************************************************/
 public class Solution {
     public bool IsNumber(string s) {
+        bool seenDigit = false; //IsDigit
+        bool seenE = false;
+        bool seenDot = false;
         
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(char.IsDigit(s[i]))
+            {
+                //digit
+                seenDigit = true;
+            }
+            else if(s[i] == '+' || s[i] == '-')
+            {
+                // + -
+                //i !=0 && !seenE  used to be wrong for "459277e38+"
+                if(i>0 && s[i - 1] != 'E' && s[i - 1] != 'e' )
+                {
+                    //not in first char or the first one after E
+                    return false;
+                }
+            }
+            else if(s[i] == 'E' || s[i] =='e')
+            {
+                //E
+                if(!seenDigit || seenE)
+                {
+                    return false;
+                }
+                //used to forgot to reset these
+                //and it's wrong for "0e"
+                seenDigit = false;
+                seenE = true;
+            }
+            else if(s[i] == '.')
+            {
+                //Dot
+                if(seenE || seenDot)
+                {
+                    return false;
+                }
+                
+                //used to forgot to rest seenDot
+                //and it's wrong for "..2"
+                seenDot = true;
+            }
+            else
+            {
+                //other char
+                return false;
+            }
+
+        }
+        
+        return seenDigit;
     }
 }
