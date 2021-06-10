6d0d86b04b93f1b90d702ce5a2a86e673bab81f9
John Zhang <John_Zhang@servms.com>
2021-06-10 15:53:02 -0500

    add Q24 and one not work solution

--- LinkedList/24.Swap_Nodes_in_Pairs.cs
+++ LinkedList/24.Swap_Nodes_in_Pairs.cs
@@ -1,12 +1,13 @@
 /*
-
+Given a linked list, swap every two adjacent nodes and return its head. 
+You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
 */
 
  /********************************************************************************
  Solution Category: 0
  *********************************************************************************
  Time Range:
- From: 
+ From: 2021-06-10 15:32
  To: 
  *********************************************************************************
  Submission Result:
@@ -15,3 +16,41 @@
  Note: 
 
  *********************************************************************************/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode SwapPairs(ListNode head) {
+        if(head.next == null || head.next.next == null) return null;
+        
+        //prev left right next
+        ListNode newhead = head.next;
+
+        ListNode prevNode = null;
+        ListNode leftNode = head;
+        ListNode rightNode = head.next;
+        
+        while(leftNode != null && rightNode != null)
+        {
+            //switch           
+            ListNode nextNode = rightNode.next;
+            prevNode.next = rightNode;
+            rightNode.next = leftNode;
+            leftNode.next = nextNode;
+            
+            //move to next
+            leftNode = leftNode.next;
+            rightNode = leftNode.next.next;
+        }
+        
+        return newhead;
+    }
+}
