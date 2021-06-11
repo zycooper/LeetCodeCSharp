692f80c24618e87fc7728e83da2df310568ee6de
John Zhang <John_Zhang@servms.com>
2021-06-11 09:41:30 -0500

    add A24

--- LinkedList/24.Swap_Nodes_in_Pairs.cs
+++ LinkedList/24.Swap_Nodes_in_Pairs.cs
@@ -14,7 +14,9 @@ You must solve the problem without modifying the values in the list's nodes (i.e
 
  *********************************************************************************
  Note: 
+Dont' initial a listnode to null like ListNode node = null, use this ListNode node = new ListNode();
 
+and get more familier about the head inside while
  *********************************************************************************/
 /**
  * Definition for singly-linked list.
@@ -27,6 +29,52 @@ You must solve the problem without modifying the values in the list's nodes (i.e
  *     }
  * }
  */
+
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode SwapPairs(ListNode head) {
+        //working version
+        //if(head.next == null || head.next.next == null) return new ListNode();
+        
+        //prev left right next
+        ListNode newhead = new ListNode();
+        newhead.next = head;
+        
+        ListNode prevNode = newhead;
+        
+        while(head != null && head.next != null)
+        {
+            ListNode leftNode = head;
+            ListNode rightNode = head.next;
+            //switch           
+            //ListNode nextNode = rightNode.next;
+            
+            prevNode.next = rightNode;
+            leftNode.next = rightNode.next;
+            rightNode.next = leftNode;
+            
+            //move to next
+            prevNode = leftNode;
+            // leftNode = leftNode.next;
+            // rightNode = leftNode.next.next;
+            head = leftNode.next;
+        }
+        
+        return newhead.next;
+    }
+}
+
+
 public class Solution {
     public ListNode SwapPairs(ListNode head) {
         if(head.next == null || head.next.next == null) return null;
@@ -34,7 +82,7 @@ public class Solution {
         //prev left right next
         ListNode newhead = head.next;
 
-        ListNode prevNode = null;
+        ListNode prevNode = new ListNode();
         ListNode leftNode = head;
         ListNode rightNode = head.next;
         
