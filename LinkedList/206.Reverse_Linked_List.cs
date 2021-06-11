5eca15ef3fd25cc37fe6d434dd16e5031041e050
John Zhang <John_Zhang@servms.com>
2021-06-11 10:45:17 -0500

    add recursion solution for 206

--- LinkedList/206.Reverse_Linked_List.cs
+++ LinkedList/206.Reverse_Linked_List.cs
@@ -28,6 +28,38 @@ Memory Usage: 25 MB, less than 79.27% of C# online submissions for Reverse Linke
  *     }
  * }
  */
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        /*
+        Runtime: 92 ms, faster than 70.61% of C# online submissions for Reverse Linked List.
+        Memory Usage: 25.1 MB, less than 61.26% of C# online submissions for Reverse Linked List.
+        */
+    //break point
+    if(head == null || head.next == null)
+    {
+        //head == null is for empty input
+        return head;
+    }
+    
+        ListNode temp_next = ReverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        
+        return temp_next;
+    
+    }
+}
 public class Solution {
     public ListNode ReverseList(ListNode head) {
         ListNode prev = null;;
--- LinkedList/92.Reverse_Linked_List_II.cs
+++ LinkedList/92.Reverse_Linked_List_II.cs
@@ -1,5 +1,6 @@
 /*
-
+Given the head of a singly linked list and two integers left and right where left <= right, 
+reverse the nodes of the list from position left to position right, and return the reversed list.
 */
 
  /********************************************************************************
@@ -15,3 +16,19 @@
  Note: 
 
  *********************************************************************************/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseBetween(ListNode head, int left, int right) {
+        
+    }
+}
