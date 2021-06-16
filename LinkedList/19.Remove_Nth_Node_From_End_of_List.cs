cd8d5e917ee5d9988e79e3392fdd98c6165110e3
John Zhang <John_Zhang@servms.com>
2021-06-16 14:26:16 -0500

    add A19

--- LinkedList/19.Remove_Nth_Node_From_End_of_List.cs
+++ LinkedList/19.Remove_Nth_Node_From_End_of_List.cs
@@ -7,14 +7,54 @@ Given the head of a linked list, remove the nth node from the end of the list an
  *********************************************************************************
  Time Range:
  From: 2021-06-16 16:50
- To: 
+ To: 2021-06-16 14:25
  *********************************************************************************
  Submission Result:
-
+Runtime: 80 ms, faster than 99.30% of C# online submissions for Remove Nth Node From End of List.
+Memory Usage: 25.5 MB, less than 24.25% of C# online submissions for Remove Nth Node From End of List.
  *********************************************************************************
  Note: 
-
+very important to create dummy and set dummy.next = head;
+then start with dummy since the result may be null
  *********************************************************************************/
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    //working version
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        //get length
+        int length = 0;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode cur = head;
+        while(cur != null)
+        {
+            length++;
+            cur = cur.next;
+        }
+        
+        length = length - n;
+        //this is very important to include the result null
+        cur = dummy;
+        while(length > 0)
+        {
+            length--;
+            cur = cur.next;
+        }
+        cur.next = cur.next.next;
+        
+        return dummy.next;
+    }
+}
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -28,6 +68,7 @@ Given the head of a linked list, remove the nth node from the end of the list an
  */
 public class Solution {
     public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        //not work on [1] 1
         //get length
         int length = 0;
         ListNode cur = head;
@@ -40,15 +81,17 @@ public class Solution {
         ListNode dummy = new ListNode();
         dummy.next = head;
         //actual position
-        int target = length - n + 1;
+        int target = length - n;
+        Console.WriteLine(target);
         cur = head;
         for(int i = 0; i < target;i++)
         {
-            cur = cur.next;
-            if(i == target -1)
+            if(i == target - 1 || target < 0)
             {
+                Console.WriteLine(cur.val);
                 cur.next = cur.next.next;
             }
+            cur = cur.next;
         }
         
         return dummy.next;
