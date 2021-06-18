0bd60bcaf1ae15adf355233ff0e0694f5f41c0aa
zycooper <yuanzhang998@gmail.com>
2021-06-18 15:33:08 -0500

    Update 148.Sort_List.cs

--- LinkedList/148.Sort_List.cs
+++ LinkedList/148.Sort_List.cs
@@ -1,5 +1,6 @@
 /*
-
+Given the head of a linked list, return the list after sorting it in ascending order.
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
 */
 
  /********************************************************************************
