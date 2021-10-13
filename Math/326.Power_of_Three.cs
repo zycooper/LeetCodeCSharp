9906e9ef0f8c529f2c7779591b140298eea821e9
zycooper <yuanzhang998@gmail.com>
2021-10-12 21:47:29 -0500

    add mindmap

--- Math/326.Power_of_Three.cs
+++ Math/326.Power_of_Three.cs
@@ -1,4 +1,7 @@
 /*
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3x.
 
 */
 
@@ -6,7 +9,7 @@
  Solution Category: 0
  *********************************************************************************
  Time Range:
- From: 
+ From: 2021-08-30 14:23
  To: 
  *********************************************************************************
  Submission Result:
@@ -15,3 +18,26 @@
  Note: 
 
  *********************************************************************************/
+public class Solution {
+    public bool IsPowerOfThree(int n) {
+        /*
+        //pre dondition
+        if(n.ToString().Length == 1)
+        {
+            if(n%3 ==0 )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //normal condition
+
+        //convert this n to part sum
+
+        */
+    }
+}
