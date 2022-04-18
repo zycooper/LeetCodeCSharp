3dfe195f88f0d45e6bd6f70b60f2a15a6200e0b3
Yuan Zhang <newbee720@gmail.com>
2022-04-18 15:38:58 -0500

    first sliding window

---
+++ Java/Sliding_Window.java
@@ -0,0 +1,38 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        //pre condition - check if nums lengh is less than k
+        //normal condition
+        //if return min, set max to it
+        //final result
+        int LargestSum = Integer.MIN_VALUE; // Interger.MAX_VALUE;
+
+        //set left cursor
+        int Window_Cursor_Left = 0;
+
+        //temporary or middle value, used to compare with the final result
+        int Current_Sum = 0;
+
+        //in Java, use lenght, all lower case
+        //set right cursor as i in for loop, every time right cursor move, but left cursor may not since if the window length is not valid
+        for(int Window_Cursor_Right = 0;  Window_Cursor_Right < nums.length; Window_Cursor_Right++)
+        {
+            //reset the temporary value, but don't have to touch the final result yet
+            Current_Sum += nums[Window_Cursor_Right];
+
+            //check if the window lengh is valid -> note it should be right_cursor >= k - 1
+            if(Window_Cursor_Right >= k - 1)
+            {
+                //reset the final result first
+                LargestSum = Math.max(Current_Sum,LargestSum);
+
+                //then reset the temporary value
+                Current_Sum -= nums[Window_Cursor_Left];
+
+                //left curos moves as well.
+                Window_Cursor_Left++;
+            }
+        }
+
+        return (double)LargestSum/k;
+    }
+}
---
+++ Java/Sliding_Window.md
@@ -0,0 +1,3 @@
+| # | Title | Difficulty | Category | Submit |
+|---| ----- | ---------- | -------- | ------ |
+|643|[Maximum Average Subarray I](https://leetcode.com/problems/maximum-average-subarray-i/)|Easy|Sliding Window|4-18-2022|
